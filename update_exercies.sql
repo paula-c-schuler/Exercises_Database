bef9092210ef168a40ded8e6599c211bbd5902ee
Paula C Schuler <paulasatx@gmail.com>
2015-03-31 18:48:42 -0500

    Starting Update exercise.

---
+++ update_exercies.sql
@@ -0,0 +1,24 @@
+USE codeup_test_db;
+
+
+Write SELECT statements to output each of the following with a caption:
+SELECT All albums in your table.
+UPDATE Make all the albums 10 times more popular (sales * 10)
+SELECT to see results
+SELECT * FROM albums;
+WHERE 
+UPDATE 
+
+
+All albums released before 1980
+UPDATE Move all the albums before 1980 back to the 1800s.
+SELECT to see results
+
+
+
+All albums by Michael Jackson
+UPDATE Change "Michael Jackson" to "Peter Jackson"
+SELECT to see results
+
+
+
