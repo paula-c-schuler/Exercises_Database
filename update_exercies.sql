b5e147e22695b83b7a87ddc4642ff82eeec3ec74
Paula C Schuler <paulasatx@gmail.com>
2015-04-02 00:46:58 -0500

    UPDATE exercises 8.4.3, one part unfinished.

--- update_exercies.sql
+++ update_exercies.sql
@@ -1,24 +1,45 @@
+-- EXERCISE 8.4.3 INCOMPLETE
+
 USE codeup_test_db;
 
 
-Write SELECT statements to output each of the following with a caption:
-SELECT All albums in your table.
-UPDATE Make all the albums 10 times more popular (sales * 10)
-SELECT to see results
+-- DONE WORKING
+-- SELECT All albums in your table.
+-- UPDATE Make all the albums 10 times more popular (sales * 10)
+-- SELECT to see results
 SELECT * FROM albums;
-WHERE 
-UPDATE 
+SET sales_in_millions = sales_in_millions*10;
+SELECT sales_in_millions FROM albums;
 
 
-All albums released before 1980
-UPDATE Move all the albums before 1980 back to the 1800s.
+-- All albums released before 1980
 SELECT to see results
+SELECT name
+FROM albums
+WHERE release_d < 1980;
+-- UPDATE Move all the albums before 1980 back to the 1800s.
+UPDATE release_d
+SET release_d = 1800
+WHERE release_d < 1980;
+
+***************************** asked question stackoverflow *****
 
 
-
-All albums by Michael Jackson
-UPDATE Change "Michael Jackson" to "Peter Jackson"
-SELECT to see results
-
-
-
+-- DONE, WORKING
+-- Show all albums by Michael Jackson
+SELECT name 
+FROM albums
+WHERE artist = 'Michael Jackson';
+-- UPDATE to change Michael Jackson to Peter Jackson
+UPDATE albums
+SET artist = 'Peter Jackson'
+WHERE artist = 'Michael Jackson';
+-- test to verify Peter Jackson since display still showed Michael Jackson
+-- verified correct! 
+SELECT artist
+FROM albums
+WHERE NAME = 'Thriller';
+-- better code
+SELECT artist
+FROM albums
+WHERE NAME = 'Thriller' OR 'Bad' OR 'Dangerous';
